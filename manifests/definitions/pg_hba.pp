c354271eeadccd03fd192c99915934ef1a050114
root <root@puppet.ebc>
2010-12-10 18:32:43 -0200

    Fix typo

--- manifests/definitions/pg_hba.pp
+++ manifests/definitions/pg_hba.pp
@@ -1,10 +1,10 @@
 define postgres::pg_hba() {
     File <| title == 'pg_hba' |> {
         source => [
-            "puppet:///files/postgres/host/pg_jba.conf.$title.$fqdn",
-            "puppet:///files/postgres/host/pg_jba.conf.$title.$hostname",
-            "puppet:///files/postgres/env/pg_jba.conf.$title.$environment",
-            "puppet:///files/postgres/pg_jba.conf.$title",
+            "puppet:///files/postgres/host/pg_hba.conf.$title.$fqdn",
+            "puppet:///files/postgres/host/pg_hba.conf.$title.$hostname",
+            "puppet:///files/postgres/env/pg_hba.conf.$title.$environment",
+            "puppet:///files/postgres/pg_hba.conf.$title",
             "puppet:///modules/postgres/pg_hba.conf.$title",
         ],
     }
